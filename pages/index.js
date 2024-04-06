dd92d564e861807894d5dccbf1681a96bd7933c7
Ainun Najib <ainunnajib@gmail.com>
2024-04-06 22:54:17 +0800

    initialize Kawal Haji

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Kawal Haji</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Bismillah Kom..." />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Bismillah Kom... puter sedang bekerja membangun aplikasi pelaporan masalah dan update situasi Jamaah Haji Indonesia 2024
         </p>
       </main>
 
